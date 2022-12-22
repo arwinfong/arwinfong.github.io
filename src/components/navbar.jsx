08f10c6565acd2d3838e5af153016731d9ae54d9
duccie42 <76762011+arwinfong@users.noreply.github.com>
2022-12-22 11:16:58 -0500

    Created starter code for website

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import "../App.css"
+
+function Navbar() {
+    return (
+        <nav>
+            <h1 className="navbar-title">This is a navbar</h1>
+            <ul className="navbar-items">
+                <li>About</li>
+                <li>Stuff</li>
+                <li>Coming Soon..</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
